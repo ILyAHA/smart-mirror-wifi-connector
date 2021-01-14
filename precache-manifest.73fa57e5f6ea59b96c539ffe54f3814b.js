e26dd465730b0c2a476db1b359a71051f96391a1
Ilya Vinogradov <i.vinogradov@cometrica.ru>
2021-01-15 02:38:15 +0300

    Updates

---
+++ precache-manifest.73fa57e5f6ea59b96c539ffe54f3814b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f6544f822527cf629fc26c0d2b51fc89",
+    "url": "/smart-mirror-wifi-connector/index.html"
+  },
+  {
+    "revision": "ddf3d08aadfc869ef1b9",
+    "url": "/smart-mirror-wifi-connector/static/css/main.d542c90d.chunk.css"
+  },
+  {
+    "revision": "f2c93d5295ad4fb08208",
+    "url": "/smart-mirror-wifi-connector/static/js/2.e530e4f3.chunk.js"
+  },
+  {
+    "revision": "f52df9ee46b4c0ec889ce4e37873fa2f",
+    "url": "/smart-mirror-wifi-connector/static/js/2.e530e4f3.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "ddf3d08aadfc869ef1b9",
+    "url": "/smart-mirror-wifi-connector/static/js/main.70803587.chunk.js"
+  },
+  {
+    "revision": "74565813181432ac8394",
+    "url": "/smart-mirror-wifi-connector/static/js/runtime-main.6e62c321.js"
+  }
+]);
