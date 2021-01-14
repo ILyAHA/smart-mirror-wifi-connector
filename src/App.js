30f0e5d4684975b8bce045a68b718c706ba88594
ILyAHA <ilich.vin@gmail.com>
2021-01-14 15:12:45 +0300

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,62 @@
+import React from "react";
+import "./styles.css";
+import { TextField } from "@material-ui/core";
+import QRCode from "react-qr-code";
+import Typography from "@material-ui/core/Typography";
+
+export default class App extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      networkName: "error",
+      networkPassword: "error"
+    };
+  }
+
+  qrString() {
+    return `SMART-MIRROR-WIFISETTINGS:NETWORK="${this.state.networkName}"&PASSWORD="${this.state.networkPassword}"`;
+  }
+
+  handleChangeName(e) {
+    this.setState({ networkName: e.target.value });
+  }
+
+  handleChangePassword(e) {
+    this.setState({ networkPassword: e.target.value });
+  }
+
+  isValidQRCode() {
+    return this.state.networkName && this.state.networkName !== "error";
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <div id="qrCode">
+          {this.isValidQRCode() ? (
+            <QRCode value={this.qrString()} />
+          ) : (
+            <Typography component="h1" variant="h5">
+              Начните вводить название Wifi сети
+            </Typography>
+          )}
+        </div>
+        <form noValidate autoComplete="off">
+          <div>
+            <TextField
+              label="Навание сети"
+              onChange={this.handleChangeName.bind(this)}
+            />
+          </div>
+          <div>
+            <TextField
+              label="Пароль"
+              type="password"
+              onChange={this.handleChangePassword.bind(this)}
+            />
+          </div>
+        </form>
+      </div>
+    );
+  }
+}
